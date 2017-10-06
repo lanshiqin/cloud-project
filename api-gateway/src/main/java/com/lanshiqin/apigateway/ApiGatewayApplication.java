18f842609282999f9b9d272db3c06ed4f6f992bc
lanshiqin <lanshiqin@outlook.com>
2017-10-06 23:57:26 +0800

    初始化项目,添加API网关和服务注册中心模块

---
+++ api-gateway/src/main/java/com/lanshiqin/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.lanshiqin.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}

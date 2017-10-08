78e2cb693f88ec73c43a9f17b695c3d1a4a2b3cc
lanshiqin <lanshiqin@outlook.com>
2017-10-08 23:02:04 +0800

    添加断容器与监控面板模块

---
+++ breaker-dashboard/src/main/java/com/lanshiqin/breakerdashboard/BreakerDashboardApplication.java
@@ -0,0 +1,20 @@
+package com.lanshiqin.breakerdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class BreakerDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakerDashboardApplication.class, args);
+	}
+}

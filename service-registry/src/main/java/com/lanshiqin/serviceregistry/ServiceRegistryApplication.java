b21e569a9fda5a5a34a56b9f09556156039ad811
lanshiqin <lanshiqin@outlook.com>
2017-10-09 17:42:14 +0800

    修改服务注册中心配置

--- service-registry/src/main/java/com/lanshiqin/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/lanshiqin/serviceregistry/ServiceRegistryApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 服务注册中心
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {

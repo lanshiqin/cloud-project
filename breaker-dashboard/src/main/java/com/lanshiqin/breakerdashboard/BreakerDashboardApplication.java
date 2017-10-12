9d1b523d416b8c247ba169e80cee25ae940ea5d4
lanshiqin <lanshiqin@outlook.com>
2017-10-12 21:58:18 +0800

    熔断器模块添加@EnableHystrix注解，启用Hystrix

--- breaker-dashboard/src/main/java/com/lanshiqin/breakerdashboard/BreakerDashboardApplication.java
+++ breaker-dashboard/src/main/java/com/lanshiqin/breakerdashboard/BreakerDashboardApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 public class BreakerDashboardApplication {

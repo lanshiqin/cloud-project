3e4c8bb9a87644f7fd7f3a38de3635560c947a6b
lanshiqin <lanshiqin@outlook.com>
2017-10-14 23:08:34 +0800

    添加服务注册中心集群配置

--- service-registry/src/main/java/com/lanshiqin/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/lanshiqin/serviceregistry/ServiceRegistryApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 服务注册中心
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer	// 启用Eureka服务
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {

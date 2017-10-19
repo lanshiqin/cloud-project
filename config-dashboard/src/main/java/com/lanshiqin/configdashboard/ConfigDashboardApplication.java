60b7c6c43c3575ee70459d641934c3c9adff47c6
lanshiqin <lanshiqin@outlook.com>
2017-10-19 15:51:19 +0800

    配置中心集群配置实现高可用

--- config-dashboard/src/main/java/com/lanshiqin/configdashboard/ConfigDashboardApplication.java
+++ config-dashboard/src/main/java/com/lanshiqin/configdashboard/ConfigDashboardApplication.java
@@ -2,10 +2,17 @@ package com.lanshiqin.configdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 配置中心
+ * @ EnableConfigServer 启用配置服务
+ * @ EnableDiscoveryClient 启用发现客户端服务
+ */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigDashboardApplication {
 
 	public static void main(String[] args) {

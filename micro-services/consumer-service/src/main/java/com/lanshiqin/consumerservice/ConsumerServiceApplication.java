e1e9cd79742eae2af3a8f3bf95dc6d4b3c3bda17
lanshiqin <lanshiqin@outlook.com>
2017-10-25 08:28:44 +0800

    消费者服务开启熔断监控

--- micro-services/consumer-service/src/main/java/com/lanshiqin/consumerservice/ConsumerServiceApplication.java
+++ micro-services/consumer-service/src/main/java/com/lanshiqin/consumerservice/ConsumerServiceApplication.java
@@ -2,17 +2,23 @@ package com.lanshiqin.consumerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * 服务调用者
  * @ EnableDiscoveryClient 开启客户端发现
  * @ EnableFeignClients 开启Feign远程调用
+ * @ EnableHystrixDashboard 开启熔断监控仪表盘
+ * @ EnableCircuitBreaker 开启断路器
  */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ConsumerServiceApplication {
 
 	public static void main(String[] args) {

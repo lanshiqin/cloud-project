0b8be7529765cf1acdc67c7c0e8018a8bb59672c
lanshiqin <lanshiqin@outlook.com>
2017-10-29 19:17:32 +0800

    添加熔断监控面板，删除out输出文件的版本控制

--- breaker-dashboard/src/main/java/com/lanshiqin/breakerdashboard/BreakerDashboardApplication.java
+++ breaker-dashboard/src/main/java/com/lanshiqin/breakerdashboard/BreakerDashboardApplication.java
@@ -5,15 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * 熔断监控
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableTurbine
 public class BreakerDashboardApplication {
 
 	public static void main(String[] args) {
